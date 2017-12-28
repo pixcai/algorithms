a79fdf002c88e253fe14fbaed556b0315c765539
pixcai <pixcai@163.com>
2017-12-28 21:26:24 +0800

    Add: leetcode/003.LongestSubstringWithoutRepeatingCharacters

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Algorithms notes and code and leetcode solution by JavaScript
 ### leetcode
 - [001.TwoSum](leetcode/001.TwoSum)
 - [002.AddTwoNumbers](leetcode/002.AddTwoNumbers)
+- [003.LongestSubstringWithoutRepeatingCharacters](leetcode/003.LongestSubstringWithoutRepeatingCharacters)
---
+++ leetcode/003.LongestSubstringWithoutRepeatingCharacters/003.LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,52 @@
+// 解法一
+function lengthOfLongestSubstring1(s) {
+  const sub = new Set([...s])
+  const N = s.length
+  let len = sub.size
+
+  if (sub.size !== N) {
+    len = 0
+
+    for (let i = 0; i < N; i++) {
+      sub.clear()
+      sub.add(s[i])
+      for (let j = i + 1; j < N; j++) {
+        if (sub.has(s[j])) {
+          break
+        } else {
+          sub.add(s[i])
+        }
+      }
+      if (sub.size > len) {
+        len = sub.size
+      }
+    }
+  }
+
+  return len
+}
+
+// 解法二
+function lengthOfLongestSubstring2(s) {
+  let sub = ''
+  let len = 0
+
+  for (let i = 0; i < s.length; i++) {
+    const j = sub.indexOf(s[i])
+
+    if (j === -1) {
+      sub += s[i]
+    } else {
+      if (sub.length > len) {
+        len = sub.length
+      }
+      sub = sub.substr(j + 1) + s[i]
+    }
+  }
+  if (sub.length > len) {
+    len = sub.length
+  }
+
+  return len
+}
+
